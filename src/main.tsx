19cc28b13993059890823acc629f873d66e310d4
LvidSolutions <solutions@wwwlvid.com>
2026-06-11 15:18:02 +0200

    Make React entry tolerant of missing root

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from "react-dom/client";
 import { App } from "./pages/App";
 import "./styles/global.css";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root") ?? document.body.appendChild(document.createElement("div"));
+rootElement.id = "root";
+
+createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
